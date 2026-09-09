a5ac55303008510a5ad2fbb2f3c948e1098830a2
byui-dev <okdiphala@hotmail.com>
2026-09-09 13:16:59 +0200

    Add header component

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function Header() {
+    return (
+        <header className="bg-blue-600 text-white py-4 shadow-md">
+            <div id="header-title" className="text-2x1 font-bold">Obakeng Diphala</div>
+            <nav className="max-w-4xl mx-auto px-4 flex justify-between items-center">
+                <ul className="flex gap-6">
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/">About</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );  
+}
